02a455c370cc3a6aa135d1d75d8f65862b447066
zaqy-ramadhan <ramadhanz619@gmail.com>
2024-09-06 19:47:26 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'city' => 'Surabaya'
-        ]);
-        
         $this->call(UserSeeder::class); 
         $this->call(StoresSeeder::class);
         $this->call(ProductsSeeder::class);
